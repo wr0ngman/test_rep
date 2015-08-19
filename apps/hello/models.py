ec0dd51986a3d736aeb902e9b73e4426378df1c4
wr0ngman <wrongmanua@gmail.com>
2015-08-19 10:22:53 +0300

    ticket:1 change models.py

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class info(models.Model)
+	name = models.CharField(max_length='250')
+	def __str__(self):
+		return u'$ $'%('Info about:',self.name)
