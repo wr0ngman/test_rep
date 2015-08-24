16471ac88aab49acf3f2ee3c57acb89ee00ae1ba
Roma <wrongmanua@gmail.com>
2015-08-24 22:09:12 +0300

    Update models.py

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class info(models.Model)
-	name = models.CharField(max_length='250')
-	def __str__(self):
-		return u'$ $'%('Info about:',self.name)
